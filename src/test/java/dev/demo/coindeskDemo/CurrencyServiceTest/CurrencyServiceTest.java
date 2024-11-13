03f084febed929b98e3e8f11e69df0d1004a6c94
Egbert <timmy12335@gmail.com>
2024-11-13 16:20:25 +0800

    fix: update mockmvc to unit test

--- src/test/java/dev/demo/coindeskDemo/CurrencyServiceTest/CurrencyServiceTest.java
+++ src/test/java/dev/demo/coindeskDemo/CurrencyServiceTest/CurrencyServiceTest.java
@@ -1,5 +1,6 @@
 package dev.demo.coindeskDemo.CurrencyServiceTest;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import dev.demo.coindeskDemo.common.CoinDeskApiService;
 import dev.demo.coindeskDemo.controller.CurrencyController;
@@ -7,133 +8,112 @@ import dev.demo.coindeskDemo.entity.Currency;
 import dev.demo.coindeskDemo.model.coindesk.BpiCurrency;
 import dev.demo.coindeskDemo.model.coindesk.CoinDeskApiResBean;
 import dev.demo.coindeskDemo.model.coindesk.UpdateTime;
+import dev.demo.coindeskDemo.model.currency.CurrencyReqBean;
 import dev.demo.coindeskDemo.model.currencyInfo.CurrencyInfo;
 import dev.demo.coindeskDemo.model.currencyInfo.CurrencyInfoResBean;
-import dev.demo.coindeskDemo.repository.CurrencyRepository;
 import dev.demo.coindeskDemo.service.CurrencyInfoService;
+import dev.demo.coindeskDemo.service.CurrencyService;
 import lombok.extern.apachecommons.CommonsLog;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.http.MediaType;
-import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.MvcResult;
 
 import java.math.BigDecimal;
-import java.nio.charset.StandardCharsets;
 import java.util.*;
 
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest
 @ExtendWith(MockitoExtension.class)
-@AutoConfigureMockMvc
 @CommonsLog
 public class CurrencyServiceTest {
-    @Autowired
-    private MockMvc mockMvc;
 
-    @MockBean
-    private CurrencyRepository currencyRepository;
-
-    @MockBean
+    @Mock
     private CoinDeskApiService coinDeskApiService;
 
-    @MockBean
+    @Mock
     private CurrencyInfoService currencyInfoService;
+
+    @Mock
+    private CurrencyService currencyService;
 
     @InjectMocks
     private CurrencyController currencyController;
 
-    @Autowired
-    private ObjectMapper objectMapper;
+    ObjectMapper objectMapper = new ObjectMapper();
 
     // 1. 測試查詢幣別對應表資料 API，並顯示其內容
     @Test
-    public void testGetAllCurrencies() throws Exception {
+    public void testGetAllCurrencies() throws JsonProcessingException {
         List<Currency> currencies = Arrays.asList(
                 new Currency("USD", "美元"),
                 new Currency("EUR", "歐元")
         );
 
-        when(currencyRepository.findAll()).thenReturn(currencies);
+        when(currencyService.getAll()).thenReturn(currencies);
 
-        MvcResult result = mockMvc.perform(get("/currency")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$[0].code").value("USD"))
-                .andExpect(jsonPath("$[0].name").value("美元"))
-                .andExpect(jsonPath("$[1].code").value("EUR"))
-                .andExpect(jsonPath("$[1].name").value("歐元"))
-                .andReturn();
+        List<Currency> response = currencyController.getAllCurrencies();
 
-        log.info(result.getResponse().getContentAsString(StandardCharsets.UTF_8));
+        assertNotNull(response);
+        assertEquals(2, response.size());
+        assertEquals("USD", response.get(0).getCode());
+        assertEquals("美元", response.get(0).getName());
+
+        log.info(objectMapper.writeValueAsString(response));
     }
 
     // 2. 測試新增幣別對應表資料 API
     @Test
-    public void testAddCurrency() throws Exception {
+    public void testAddCurrency() {
         Currency currency = new Currency("GBP", "英鎊");
-        when(currencyRepository.save(any(Currency.class))).thenReturn(currency);
+        when(currencyService.saveCurrency(any(CurrencyReqBean.class))).thenReturn(currency);
 
-        mockMvc.perform(post("/currency")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"code\": \"GBP\", \"name\": \"英鎊\"}"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.code").value("GBP"))
-                .andExpect(jsonPath("$.name").value("英鎊"));
+        Currency response = currencyController.saveCurrency(new CurrencyReqBean("GBP", "英鎊"));
+
+        assertNotNull(response);
+        assertEquals("GBP", response.getCode());
+        assertEquals("英鎊", response.getName());
     }
 
     // 3. 測試更新幣別對應表資料 API，並顯示其內容
     @Test
-    public void testUpdateCurrency() throws Exception {
+    public void testUpdateCurrency() throws JsonProcessingException {
         Currency existingCurrency = new Currency("USD", "美元");
-        when(currencyRepository.findById("USD")).thenReturn(Optional.of(existingCurrency));
-
+        when(currencyService.findById("USD")).thenReturn(Optional.of(existingCurrency));
 
         Currency updatedCurrency = new Currency("USD", "美金");
-        when(currencyRepository.save(any(Currency.class))).thenReturn(updatedCurrency);
+        when(currencyService.saveCurrency(any(CurrencyReqBean.class))).thenReturn(updatedCurrency);
 
-        MvcResult result = mockMvc.perform(put("/currency")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("{\"code\": \"USD\", \"name\": \"美金\"}"))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("$.code").value("USD"))
-                .andExpect(jsonPath("$.name").value("美金"))
-                .andReturn();
+        Currency response = currencyController.updateCurrency(new CurrencyReqBean("USD", "美金"));
 
-        log.info(result.getResponse().getContentAsString(StandardCharsets.UTF_8));
+        assertNotNull(response);
+        assertEquals("USD", response.getCode());
+        assertEquals("美金", response.getName());
+
+        log.info(objectMapper.writeValueAsString(response));
     }
 
     // 4. 測試刪除幣別對應表資料 API
     @Test
-    public void testDeleteCurrency() throws Exception {
-        doNothing().when(currencyRepository).deleteById("USD");
+    public void testDeleteCurrency() {
+        doNothing().when(currencyService).deleteById("USD");
 
-        mockMvc.perform(delete("/currency/USD"))
-                .andExpect(status().isOk());
+        currencyController.deleteCurrency("USD");
 
-        verify(currencyRepository, times(1)).deleteById("USD");
+        verify(currencyService, times(1)).deleteById("USD");
     }
 
     // 5. 測試呼叫 Coindesk API，並顯示其內容
     @Test
-    public void testGetCoinDeskData() throws Exception {
+    public void testGetCoinDeskData() throws JsonProcessingException {
         CoinDeskApiResBean mockResponse = new CoinDeskApiResBean();
-        // 設置時間信息
         UpdateTime time = new UpdateTime();
         time.setUpdated("Nov 12, 2024 14:29:42 UTC");
         time.setUpdatedISO("2024-11-12T14:29:42+00:00");
         time.setUpdateduk("Nov 12, 2024 at 14:29 GMT");
 
-        // 設置幣別信息
         BpiCurrency usd = new BpiCurrency();
         usd.setCode("USD");
         usd.setSymbol("&#36;");
@@ -141,65 +121,47 @@ public class CurrencyServiceTest {
         usd.setDescription("United States Dollar");
         usd.setRateFloat(BigDecimal.valueOf(85620.5893));
 
-        BpiCurrency gbp = new BpiCurrency();
-        gbp.setCode("GBP");
-        gbp.setSymbol("&pound;");
-        gbp.setRate("66,836.031");
-        gbp.setDescription("British Pound Sterling");
-        gbp.setRateFloat(BigDecimal.valueOf(66836.0314));
-
-        BpiCurrency eur = new BpiCurrency();
-        eur.setCode("EUR");
-        eur.setSymbol("&euro;");
-        eur.setRate("80,600.483");
-        eur.setDescription("Euro");
-        eur.setRateFloat(BigDecimal.valueOf(80600.4829));
-
-        // 將幣別信息放入 Map
         Map<String, BpiCurrency> bpi = new HashMap<>();
         bpi.put("USD", usd);
-        bpi.put("GBP", gbp);
-        bpi.put("EUR", eur);
 
-        // 組裝 mockResponse
         mockResponse.setUpdateTime(time);
-        mockResponse.setDisclaimer("This data was produced from the CoinDesk Bitcoin Price Index (USD). Non-USD currency data converted using hourly conversion rate from openexchangerates.org");
+        mockResponse.setDisclaimer("Data from CoinDesk API");
         mockResponse.setChartName("Bitcoin");
         mockResponse.setBpi(bpi);
 
         when(coinDeskApiService.getCurrentPrice()).thenReturn(mockResponse);
 
-        MvcResult result = mockMvc.perform(get("/coinDesk")
-                        .contentType(MediaType.APPLICATION_JSON))
-                .andExpect(status().isOk())
-                .andExpect(content().json(objectMapper.writeValueAsString(mockResponse)))
-                .andReturn();
+        CoinDeskApiResBean response = coinDeskApiService.getCurrentPrice();
 
-        log.info(result.getResponse().getContentAsString(StandardCharsets.UTF_8));
+        assertNotNull(response);
+        assertEquals("Bitcoin", response.getChartName());
+        assertEquals("USD", response.getBpi().get("USD").getCode());
+
+        log.info(objectMapper.writeValueAsString(response));
     }
 
     // 6. 測試呼叫資料轉換的 API，並顯示其內容
     @Test
-    public void testGetTransformedData() throws Exception {
+    public void testGetTransformedData() throws JsonProcessingException {
         CurrencyInfoResBean currencyInfoResBean = new CurrencyInfoResBean();
         currencyInfoResBean.setUpdateTime("2024/11/12 08:48:50");
         CurrencyInfo currencyInfo = new CurrencyInfo();
         currencyInfo.setCode("USD");
         currencyInfo.setName("美元");
         currencyInfo.setRate(BigDecimal.valueOf(50000.0));
+
         currencyInfoResBean.setCurrencyInfo(Collections.singletonList(currencyInfo));
 
         when(currencyInfoService.getCurrencyInfo()).thenReturn(currencyInfoResBean);
 
-        MvcResult result = mockMvc.perform(get("/currency/info")
-                        .contentType(MediaType.APPLICATION_JSON))
-                        .andExpect(status().isOk())
-                        .andExpect(jsonPath("$.updateTime").value("2024/11/12 08:48:50"))
-                        .andExpect(jsonPath("$.currencyInfo[0].code").value("USD"))
-                        .andExpect(jsonPath("$.currencyInfo[0].name").value("美元"))
-                        .andExpect(jsonPath("$.currencyInfo[0].rate").value("50000.0"))
-                        .andReturn();
+        CurrencyInfoResBean response = currencyController.getCurrencyInfo();
 
-        log.info(result.getResponse().getContentAsString(StandardCharsets.UTF_8));
+        assertNotNull(response);
+        assertEquals("2024/11/12 08:48:50", response.getUpdateTime());
+        assertEquals("USD", response.getCurrencyInfo().get(0).getCode());
+        assertEquals("美元", response.getCurrencyInfo().get(0).getName());
+        assertEquals(BigDecimal.valueOf(50000.0), response.getCurrencyInfo().get(0).getRate());
+
+        log.info(objectMapper.writeValueAsString(response));
     }
 }
